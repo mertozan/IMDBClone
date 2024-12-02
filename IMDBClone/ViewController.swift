aeec877d5016ed881a714cb32a98071a21e547da
mertozan <mertozan1997@gmail.com>
2024-12-02 23:35:47 +0300

    initial commit

---
+++ IMDBClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDBClone
+//
+//  Created by Mert ÖZAN on 2.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
